32a27e023670bd9c152ff7a4e2d81718af1a11b2
Peshka564 <matpet0412@gmail.com>
2023-08-03 11:43:36 +0300

    Improved KNN to work with multiple test points

--- knn.py
+++ knn.py
@@ -1,37 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def knn(training_points, labels, testing_point, k):
-    # Find distances from testing point to each training point
+def knn(training_points, labels, testing_points, k):
+    # Find distances from each testing point to each training point
     n = len(training_points)
-    distances = np.zeros(n)
-    for i, training_point in enumerate(training_points):
-        distances[i] = np.linalg.norm(training_point - testing_point)
+    m = len(testing_points)
+    distances = np.zeros(shape=(m, n))
+    for i, testing_point in enumerate(testing_points):
+        for j, training_point in enumerate(training_points):
+            distances[i][j] = np.linalg.norm(training_point - testing_point)
     
     # Find the k smallest distances + corresponding indices
-    picked = np.zeros(n)
-    closestDistances = np.array([np.max(distances)] * k)
-    closestIndices = np.array([-1] * k)
-    for i in range(0, k):
-        for j in range(0, n):
-            if(picked[j] == 0 and closestDistances[i] >= distances[j]):
-                closestDistances[i] = distances[j]
-                closestIndices[i] = j
-        picked[closestIndices[i]] = 1
+    picked = np.zeros(shape=(m, n))
+    closestDistances = np.full(shape=(m, k), fill_value=np.max(distances))
+    closestIndices = np.full(shape=(m, k), fill_value=-1)
+    for testing_index in range(0, m):
+        for currentPosition in range(0, k):
+            for training_index in range(0, n):
+                if(picked[testing_index][training_index] == 0 and closestDistances[testing_index][currentPosition] >= distances[testing_index][training_index]):
+                    closestDistances[testing_index][currentPosition] = distances[testing_index][training_index]
+                    closestIndices[testing_index][currentPosition] = training_index
+            picked[testing_index][closestIndices[testing_index][currentPosition]] = 1
+    print(closestDistances)
+    print(closestIndices)
+    # Classify points
+    label_sums = np.zeros(shape=(m))
+    for i in range(0, m):
+        for j in range(0, k):
+            label_sums[i] += labels[closestIndices[i][j]]
+        if label_sums[i] == 0:
+            # In case of tie -> choose random            
+            label_sums[i] = np.random.choice([-1, 1], 1)
 
-    # Choose which label to classify
-    # If the number of labels for each class matches,
-    # Reduce k from even to odd
-    label_sum = 0
-    for j in range(0, k):
-        label_sum += labels[closestIndices[j]]
+            # In case of tie -> reduce k from even to odd
+            # label_sums[i] -= labels[closestIndices[i][-1]]
+            # closestIndices[i] = closestIndices[i][:-1]  
+        elif(label_sums[i] > 0): label_sums[i] = 1
+        elif(label_sums[i] < 0): label_sums[i] = -1
 
-    if label_sum == 0:
-        label_sum -= labels[closestIndices[-1]]
-        closestIndices = closestIndices[:-1]
-    
-    if(label_sum > 0): label_sum = 1
-    elif(label_sum < 0): label_sum = -1
-    #elif(label_sum == 0): label_sum = np.random.choice([-1, 1], 1)
-
-    return (label_sum, closestIndices)
+    return (label_sums, closestIndices)
--- knnPlot.py
+++ knnPlot.py
@@ -16,6 +16,7 @@ class KNN_Plotter(ClassificationPlot):
         self.color = "black"
         self.ax.set_title(self.title)
     
+    # This is knn with a single testing point
     def classify(self, testing_point):
         
         # deal with excess lines and points for previous testing point
@@ -30,14 +31,14 @@ class KNN_Plotter(ClassificationPlot):
         training_y = np.hstack((self.blue_y, self.red_y))
         training_data = np.stack((training_x, training_y), axis=1)
         labels = np.hstack((np.array([-1] * len(self.blue_x)), np.array([1] * len(self.red_x))))
-        label, indices = knn(training_data, labels, testing_point, self.k)
+        label, indices = knn(training_data, labels, np.array([testing_point]), self.k)
 
         # Drawing prediction
-        for index in indices:
+        for index in indices[0]:
             # connect points to visualize nearest neighbours
             self.temporaries.append(self.ax.plot([training_data[index][0], testing_point[0]], [training_data[index][1], testing_point[1]], color="green")[0])
         self.temporaries.append(self.ax.scatter(testing_point[0], testing_point[1], color=self.color))
-        self.title = f"The labels is: {'Blue' if label == -1 else 'Red'}"
+        self.title = f"The labels is: {'Blue' if label[0] == -1 else 'Red'}"
         self.ax.set_title(self.title)
         self.show()
     
